8ecdd1c0c27c2ce16c2b66918862c7bde9deaa9b
Katarina Soja <katarina.soja@stu.ibu.edu.ba>
2025-12-12 14:20:37 +0100

    joins in sql queries for author name member name...

--- backend/dao/BooksDao.php
+++ backend/dao/BooksDao.php
@@ -7,13 +7,25 @@ class BooksDao extends BaseDao {
         parent::__construct("books");
     }
 
+    public function get_all()
+    {
+        $query = "SELECT b.*, a.name AS author_name, c.name AS category_name FROM books b LEFT JOIN authors a ON b.author_id = a.id LEFT JOIN categories c ON b.category_id = c.id";
+        return $this->query($query, []);
+    }
+
+    public function get_by_id($id)
+    {
+        $query = "SELECT b.*, a.name AS author_name, c.name AS category_name FROM books b LEFT JOIN authors a ON b.author_id = a.id LEFT JOIN categories c ON b.category_id = c.id WHERE b.id = :id";
+        return $this->query_unique($query, ['id' => $id]);
+    }
+
     public function getBooksByAuthorId($author_id) {
-        $query = "SELECT * FROM books WHERE author_id = :author_id";
+        $query = "SELECT b.*, a.name AS author_name, c.name AS category_name FROM books b LEFT JOIN authors a ON b.author_id = a.id LEFT JOIN categories c ON b.category_id = c.id WHERE b.author_id = :author_id";
         return $this->query($query, ['author_id' => $author_id]);
     }
 
     public function getBooksByCategoryId($category_id) {
-        $query = "SELECT * FROM books WHERE category_id = :category_id";
+        $query = "SELECT b.*, a.name AS author_name, c.name AS category_name FROM books b LEFT JOIN authors a ON b.author_id = a.id LEFT JOIN categories c ON b.category_id = c.id WHERE b.category_id = :category_id";
         return $this->query($query, ['category_id' => $category_id]);
     }
 }
--- backend/dao/BorrowedBooksDao.php
+++ backend/dao/BorrowedBooksDao.php
@@ -7,13 +7,25 @@ class BorrowedBooksDao extends BaseDao {
         parent::__construct("borrowedbooks");
     }
 
+    public function get_all()
+    {
+        $query = "SELECT bb.*, u.username AS member_username, CONCAT(u.firstName, ' ', u.lastName) AS member_name, b.title AS book_title FROM borrowedbooks bb LEFT JOIN users u ON bb.user_id = u.id LEFT JOIN books b ON bb.book_id = b.id";
+        return $this->query($query, []);
+    }
+
+    public function get_by_id($id)
+    {
+        $query = "SELECT bb.*, u.username AS member_username, CONCAT(u.firstName, ' ', u.lastName) AS member_name, b.title AS book_title FROM borrowedbooks bb LEFT JOIN users u ON bb.user_id = u.id LEFT JOIN books b ON bb.book_id = b.id WHERE bb.id = :id";
+        return $this->query_unique($query, ['id' => $id]);
+    }
+
     public function getBorrowedBooksByUserId($user_id) {
-        $query = "SELECT * FROM borrowedbooks WHERE user_id = :user_id";
+        $query = "SELECT bb.*, u.username AS member_username, CONCAT(u.firstName, ' ', u.lastName) AS member_name, b.title AS book_title FROM borrowedbooks bb LEFT JOIN users u ON bb.user_id = u.id LEFT JOIN books b ON bb.book_id = b.id WHERE bb.user_id = :user_id";
         return $this->query($query, ['user_id' => $user_id]);
     }
 
     public function getBorrowedBooksByBookId($book_id) {
-        $query = "SELECT * FROM borrowedbooks WHERE book_id = :book_id";
+        $query = "SELECT bb.*, u.username AS member_username, CONCAT(u.firstName, ' ', u.lastName) AS member_name, b.title AS book_title FROM borrowedbooks bb LEFT JOIN users u ON bb.user_id = u.id LEFT JOIN books b ON bb.book_id = b.id WHERE bb.book_id = :book_id";
         return $this->query($query, ['book_id' => $book_id]);
     }
 }
