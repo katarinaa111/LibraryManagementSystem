540e6e0567ed47ec2ce085b97fc43b27464e715e
Katarina Soja <katarina.soja@stu.edu.ibu.ba>
2025-12-26 17:35:13 +0100

    production config

---
+++ backend/config.php
@@ -0,0 +1,34 @@
+<?php
+
+class Config
+{
+    public static function DB_NAME()
+    {
+        return Config::get_env('DB_NAME', 'library_db');
+    }
+    public static function DB_PORT()
+    {
+        return Config::get_env('DB_PORT', 3306);
+    }
+    public static function DB_USER()
+    {
+        return Config::get_env('DB_USER', 'root');
+    }
+    public static function DB_PASSWORD()
+    {
+        return Config::get_env('DB_PASSWORD', '');
+    }
+    public static function DB_HOST()
+    {
+        return Config::get_env('DB_HOST', '127.0.0.1');
+    }
+    public static function JWT_SECRET()
+    {
+        return Config::get_env('JWT_SECRET', 'your_key_string');
+    }
+
+    public static function get_env($name, $default)
+    {
+        return isset($_ENV[$name]) && trim($_ENV[$name]) != "" ? $_ENV[$name] : $default;
+    }
+}
