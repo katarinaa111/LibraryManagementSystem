db94f11cf7cc0efe2b0e21f8fc75951bab565b88
Katarina Soja <katarina.soja@stu.edu.ibu.ba>
2025-12-26 16:50:02 +0100

    config env

--- backend/example.config.php
+++ backend/example.config.php
@@ -4,26 +4,31 @@ class Config
 {
     public static function DB_NAME()
     {
-        return 'library_db';
+        return Config::get_env('DB_NAME', 'library_db');
     }
     public static function DB_PORT()
     {
-        return 3306;
+        return Config::get_env('DB_PORT', 3306);
     }
     public static function DB_USER()
     {
-        return 'root';
+        return Config::get_env('DB_USER', 'root');
     }
     public static function DB_PASSWORD()
     {
-        return '';
+        return Config::get_env('DB_PASSWORD', '');
     }
     public static function DB_HOST()
     {
-        return '127.0.0.1';
+        return Config::get_env('DB_HOST', '127.0.0.1');
     }
     public static function JWT_SECRET()
     {
-        return 'your_key_string';
+        return Config::get_env('JWT_SECRET', 'your_key_string');
+    }
+
+    public static function get_env($name, $default)
+    {
+        return isset($_ENV[$name]) && trim($_ENV[$name]) != "" ? $_ENV[$name] : $default;
     }
 }
