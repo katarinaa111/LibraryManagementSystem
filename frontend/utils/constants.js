d7834682a3ce4b9f7470a4fd14aa905251993ebe
Katarina Soja <katarina.soja@stu.edu.ibu.ba>
2025-12-28 17:26:03 +0100

    fix link

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -2,7 +2,7 @@ let Constants = {
   PROJECT_BASE_URL:
     location.hostname == "localhost"
       ? "http://localhost/KatarinaSoja/LibraryManagementSystem/backend"
-      : "https://library-db-katarina-soja-backend-kug26.ondigitalocean.app/",
+      : "https://library-db-katarina-soja-backend-kug26.ondigitalocean.app",
   USER_ROLE: "user",
   ADMIN_ROLE: "admin",
 };
